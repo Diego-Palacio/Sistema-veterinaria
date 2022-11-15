0bbbd30d91e99844e0991a5a66aadcd2cd17286f
Your Name <you@example.com>
2022-11-14 21:28:35 -0300

    agrego parte logica, main de la app

---
+++ src/main/java/logica/AppVeterinaria.java
@@ -0,0 +1,16 @@
+package logica;
+
+import interfaz.PantallaPrincipal;
+
+public class AppVeterinaria {
+
+		public static void main(String[] args) {
+			
+			PantallaPrincipal pantallaPrincipal= new PantallaPrincipal();
+			pantallaPrincipal.setVisible(true);
+			pantallaPrincipal.setLocationRelativeTo(null);
+			
+		}
+
+
+}
