ef451c532cd1cfec03226e64fa60332c51e62299
Your Name <you@example.com>
2022-11-14 22:26:18 -0300

    dependencia mysql connector + conexion a bdd

--- src/main/java/persistencia/controladoraPersistencia.java
+++ src/main/java/persistencia/controladoraPersistencia.java
@@ -1,5 +1,29 @@
 package persistencia;
 
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class controladoraPersistencia {
+	
+	
+	public static void main(String[] args) {
+		
+		try {
+			//driver
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			//localhost + nombre bdd + user + contraseña
+			DriverManager.getConnection("jdbc:mysql://localhost:3306/peluqueria_animal?useSSL=false","root","2Minutos2Minutos");
+			
+			System.out.println("Conexion realizada con exito!");
+		
+		} catch (ClassNotFoundException e) {
+			System.out.println("Error conexion 1");
+			e.printStackTrace();
+		}
+		catch(SQLException e) {
+			System.out.println("Error conexion 2");
+			e.printStackTrace();
+		}
+	}
 
 }
